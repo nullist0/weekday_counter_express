96d8cd9e986f91a22c1beb8489b71e74f097a310
Lee-Null <nullist.null@gmail.com>
2021-05-25 18:36:43 +0900

    add gitignore

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 5000
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+    console.log(req.query);
+  res.send(`HELLO`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
