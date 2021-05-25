d88dd8b746aa1fc199a4881d7e7d93275239ef50
Lee-Null <nullist.null@gmail.com>
2021-05-25 19:56:37 +0900

    change npm start

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express()
-const port = 5000
+const { getHolidaysIn } = require('./lib/xmlHolidayDataSource');
+const { format, parse } = require('date-fns');
 
-app.use(cors());
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.use(cors({
+  origin: "https://out-of-existence.github.io/",
+  optionsSuccessStatus: 200
+}));
 
-    console.log(req.query);
-  res.send(`HELLO`);
+app.get('/holidays', async (req, res) => {
+  const { year, month } = req.query;
+
+  if (year === undefined || month === undefined) {
+    res.status(404);
+    res.json({
+      dates: []
+    });
+  } else {
+    const dates = await getHolidaysIn(parse(`${year}/${month}/01`, 'yyyy/MM/dd', new Date()));
+    res.json({
+      dates: dates.map(date => format(date, 'yyyyMMdd'))
+    });
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port);
